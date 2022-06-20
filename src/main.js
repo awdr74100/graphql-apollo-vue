6062ab230c85ad093aed5eeedb51edb5c27f17ec
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-21 03:52:56 +0800

    Update progress

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import apolloProvider from "./apolloProvider";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(apolloProvider);
 
 app.mount("#app");
