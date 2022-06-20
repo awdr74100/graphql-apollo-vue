cd8f6d5e5c9e9679561a32e8f7e33a7600e1a59e
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-20 17:03:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="main">
+    <h1 class="align-center">測試</h1>
+    <h2 class="align-center">{{ count }}</h2>
+    <div class="align-center">
+      <button class="btn" @click.prevent="increaseCount(1)">增加</button>
+      <button class="btn" @click.prevent="decreaseCount(1)">減少</button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    increaseCount(num) {
+      this.count += num;
+    },
+    decreaseCount(num) {
+      this.count -= num;
+    },
+  },
+};
+</script>
+
+<style>
+.align-center {
+  text-align: center;
+}
+</style>
