77f1ce82b5815c7c5aeea3544b82821eb5791b35
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-23 15:09:37 +0800

    Update progress

--- src/plugins/vue-apollo-composable.js
+++ src/plugins/vue-apollo-composable.js
@@ -10,6 +10,9 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
 });
 
+/**
+ * ref: https://www.apollographql.com/docs/react/networking/advanced-http-networking/
+ */
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
 
